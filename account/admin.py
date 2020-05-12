13fe6cc9164a0787f4ccb2e1c4647dcff68d8b6a
sajib1066 <sajib1066@gmail.com>
2020-05-12 15:50:17 +0600

    create profile model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'title', 'date']
+    search_fields = ['name']
+
+admin.site.register(Profile, ProfileAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from sajibnotes.helper import get_current_user
+
+class Profile(models.Model):
+    current_user = get_current_user
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=current_user)
+    name = models.CharField(max_length=120)
+    photo = models.ImageField(upload_to='profile/')
+    title = models.CharField(max_length=240)
+    about = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
