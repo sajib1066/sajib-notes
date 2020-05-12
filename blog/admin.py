442fd6bbcd42262bf265bb534c7c73465591b055
sajib1066 <sajib1066@gmail.com>
2020-05-12 16:36:53 +0600

    create notes model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Tag, Comment, Notes
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Notes)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+from account.models import Profile
+from sajibnotes.helper import get_current_user
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=15)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField()
+    website = models.URLField(blank=True, null=True)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Notes(models.Model):
+    title = models.CharField(max_length=240)
+    slug = models.SlugField(max_length=120, unique=True)
+    photo = models.ImageField(upload_to='notes/')
+    content = RichTextUploadingField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    is_delete = models.BooleanField(default=False)
+    comment = models.ManyToManyField(Comment, blank=True)
+    current_user = get_current_user
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=current_user)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
