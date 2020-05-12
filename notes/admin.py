70a78ad261cd1403097db99287223eed26cd3971
sajib1066 <sajib1066@gmail.com>
2020-05-12 16:49:32 +0600

    update note models

--- notes/admin.py
+++ notes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Category, Tag, Comment, Notes
+from .models import Category, Tag, Comment, Note
 
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Comment)
-admin.site.register(Notes)
+admin.site.register(Note)
--- notes/models.py
+++ notes/models.py
@@ -18,7 +18,23 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+class Note(models.Model):
+    title = models.CharField(max_length=240)
+    slug = models.SlugField(max_length=120, unique=True)
+    photo = models.ImageField(upload_to='notes/')
+    content = RichTextUploadingField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    is_delete = models.BooleanField(default=False)
+    current_user = get_current_user
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=current_user)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
     name = models.CharField(max_length=120)
     email = models.EmailField()
     website = models.URLField(blank=True, null=True)
@@ -27,19 +43,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.name
-
-class Notes(models.Model):
-    title = models.CharField(max_length=240)
-    slug = models.SlugField(max_length=120, unique=True)
-    photo = models.ImageField(upload_to='notes/')
-    content = RichTextUploadingField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tag = models.ManyToManyField(Tag)
-    is_delete = models.BooleanField(default=False)
-    comment = models.ManyToManyField(Comment, blank=True)
-    current_user = get_current_user
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=current_user)
-    published_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
