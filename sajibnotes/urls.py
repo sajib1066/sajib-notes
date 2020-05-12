9d7803ee93695d5b9f0cda2d53d9a1cb7d7b0786
sajib1066 <sajib1066@gmail.com>
2020-05-12 16:00:48 +0600

    setup media url

--- sajibnotes/urls.py
+++ sajibnotes/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
