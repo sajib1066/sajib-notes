4a066fa749884ed4ef856edab593db47e905c36a
sajib1066 <sajib1066@gmail.com>
2020-05-12 16:39:35 +0600

    create notes models

--- blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Tag, Comment, Notes
-
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Comment)
-admin.site.register(Notes)
