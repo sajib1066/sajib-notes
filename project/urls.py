94061d18cac24bafc4c569a063b386c1ce91a156
sajib1066 <sajib1066@gmail.com>
2020-05-13 10:47:08 +0600

    create project details page

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProjectListView
+from .views import ProjectListView, ProjectDetailView
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='project')
+    path('', ProjectListView.as_view(), name='project'),
+    path('<slug>/', ProjectDetailView.as_view(), name='project-details')
 ]
--- project/views.py
+++ project/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Project
 
 class ProjectListView(ListView):
     queryset = Project.objects.filter(is_delete=False)
     template_name = 'project/project.html'
+
+class ProjectDetailView(DetailView):
+    queryset = Project.objects.filter(is_delete=False)
+    template_name = 'project/details.html'
