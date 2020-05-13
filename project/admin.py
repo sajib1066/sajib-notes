fc9f12bc0a0a032cd812f87e50bd02158b821e3a
sajib1066 <sajib1066@gmail.com>
2020-05-13 09:31:47 +0600

    create project models

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Tag, Project
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Project)
--- project/models.py
+++ project/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+from account.models import Profile
+from sajibnotes.helper import get_current_user
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=15)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=240)
+    slug = models.SlugField(max_length=120, unique=True)
+    photo = models.ImageField(upload_to='notes/')
+    content = RichTextUploadingField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    is_delete = models.BooleanField(default=False)
+    current_user = get_current_user
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=current_user)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
