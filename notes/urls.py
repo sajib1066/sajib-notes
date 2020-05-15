c8043b5070f2882ef6e076cdeeb27cfdb34ac82a
sajib1066 <sajib1066@gmail.com>
2020-05-15 15:57:57 +0600

    create comment

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NoteListView, NoteDetailView
+from .views import NoteListView, NoteDetailView, note_details
 
 urlpatterns = [
     path('', NoteListView.as_view(), name='note'),
-    path('<slug>/', NoteDetailView.as_view(), name='note-details'),
+    # path('<slug>/', NoteDetailView.as_view(), name='note-details'),
+    path('<slug>/', note_details, name='note-details'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import (
     ListView,
     DetailView
 )
-from .models import Note
+from .models import Note, Comment
+from .forms import CommentForm
 
 class NoteListView(ListView):
     queryset = Note.objects.filter(is_delete=False)
@@ -12,3 +13,26 @@ class NoteListView(ListView):
 class NoteDetailView(DetailView):
     queryset = Note.objects.all()
     template_name = 'notes/details.html'
+
+def note_details(request, slug):
+    note = Note.objects.get(slug=slug)
+    comment = Comment.objects.filter(note=note)
+    form = CommentForm(request.POST or None)
+    
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    website = request.POST.get('website')
+    message = request.POST.get('message')
+    if (email):
+        Comment.objects.create(note=note, name=name, email=email, website=website, message=message)
+        context = {
+            'form': form
+        }
+        return render(request, 'notes/details.html', context)
+    
+    context = {
+        'note': note,
+        'comment': comment,
+        'form': form
+    }
+    return render(request, 'notes/details.html', context)
