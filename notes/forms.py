f66a02dc313710b666f882b11ac349527c042d29
sajib1066 <sajib1066@gmail.com>
2020-05-16 07:27:04 +0600

    add comment count by notes

--- notes/forms.py
+++ notes/forms.py
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={"class": "form-control"}),
+            'email': forms.TextInput(attrs={"class": "form-control", "type": 'email'}),
+            'website': forms.TextInput(attrs={"class": "form-control", "type": "url"}),
+            'message': forms.Textarea(attrs={"class": "form-control"}),
+        }
--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,8 @@ from django.views.generic import (
     ListView,
     DetailView
 )
+from django.db.models import Count
+
 from .models import Note, Comment
 from .forms import CommentForm
 
@@ -17,6 +19,7 @@ class NoteDetailView(DetailView):
 def note_details(request, slug):
     note = Note.objects.get(slug=slug)
     comment = Comment.objects.filter(note=note)
+    coment_count = Comment.objects.filter(note=note).annotate(number_of_note=Count('note')).order_by('-number_of_note', 'date')
     form = CommentForm(request.POST or None)
     
     name = request.POST.get('name')
@@ -26,13 +29,17 @@ def note_details(request, slug):
     if (email):
         Comment.objects.create(note=note, name=name, email=email, website=website, message=message)
         context = {
-            'form': form
+            'form': form,
+            'note': note,
+            'comment': comment,
+            'count': coment_count
         }
         return render(request, 'notes/details.html', context)
     
     context = {
         'note': note,
         'comment': comment,
-        'form': form
+        'form': form,
+        'count': coment_count
     }
     return render(request, 'notes/details.html', context)
