5b5b5a87bc44ee0af55c19eb268e9541f825e35c
sajib1066 <sajib1066@gmail.com>
2020-07-10 11:30:12 +0600

    update about page

--- sajibnotes/views.py
+++ sajibnotes/views.py
@@ -3,13 +3,19 @@ from django.views.generic import ListView, DetailView
 from contactforms.forms import ContactForm
 
 from notes.models import Note
+from account.models import Profile
 
 class HomePage(ListView):
     queryset = Note.objects.filter(is_delete=False)
     template_name = 'index.html'
 
 def about_page(request):
-    return render(request, 'about.html')
+    try:
+        profile = Profile.objects.get(user=request.user)
+        context = {'profile': profile}
+        return render(request, 'about.html', context)
+    except:
+        return render(request, 'about.html')
 
 def contact_page(request):
     forms = ContactForm()
